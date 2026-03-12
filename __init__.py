60818f01602f19b04dbe9dc02ae287e3b2b8b4e6
Abdellatif Nahal <abdnahal@e1r1p4.1337.ma>
2026-03-12 19:30:17 +0000

    added __init__ file

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from ex0 import Card, CreatureCard
+from ex1 import ArtifactCard, Deck, SpellCard
+from ex2 import Combatable, EliteCard, Magical
+from ex3 import AggressiveStrategy, CardFactory, FantasyCardFactory
+from ex3 import GameEngine, GameStrategy
+from ex4 import Rankable, TournamentCard, TournamentPlatform
+
+__all__ = ["Card", "CreatureCard", "ArtifactCard", "Deck", "SpellCard",
+           "Combatable", "EliteCard", "Magical", "AggressiveStrategy",
+           "CardFactory", "FantasyCardFactory", "GameEngine",
+           "GameStrategy", "Rankable", "TournamentCard", "TournamentPlatform"]
